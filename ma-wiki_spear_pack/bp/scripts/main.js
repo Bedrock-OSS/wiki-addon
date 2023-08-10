3f34dd9e36589a8a62db7f5351b62416c06bf5fe
MJ105 <87320205+MedicalJewel105@users.noreply.github.com>
2023-08-11 00:17:49 +0300

    Add spear add-on.

---
+++ ma-wiki_spear_pack/bp/scripts/main.js
@@ -0,0 +1,36 @@
+import { world, ItemStack, ItemTypes } from "@minecraft/server"
+import { system } from "@minecraft/server";
+//This prevents world crash
+system.events.beforeWatchdogTerminate.subscribe(data => {
+  data.cancel = true;
+});
+
+world.events.itemReleaseCharge.subscribe(ev => {
+    //This is for multiplayer support
+    for (const player of world.getPlayers()){
+    //Basic variables to get the player inventory and held item.
+      let inv = player.getComponent( 'inventory' ).container
+      //Our ItemStact to save our item. This also saves item data.
+      const itemStack = inv.getItem(player.selectedSlot);
+    //If the item we're holding is our spear, we run code.
+      if (itemStack?.typeId === 'wiki:iron_spear') {
+        var container = player.getComponent('inventory').container
+        //The new item to be given.
+        var newItem =  new ItemStack(ItemTypes?.get("wiki:iron_spear"));
+        var oldItem = container?.getItem(player.selectedSlot)
+        //Here's that tag!
+        player.removeTag("iron_spear")
+        }
+        //We subscribe a tick event to detect when we have the tag and if the item has durability less than the max.
+      let e = world.events.tick.subscribe(data => {
+      if(player.hasTag("iron_spear") && itemStack?.typeId === 'wiki:iron_spear' && itemStack?.getComponent("durability").damage <= 125) {
+        player.removeTag("iron_spear")
+        //This gives our saved item (newItem) +1 durability each time we pick it up.
+        newItem.getComponent("durability").damage = oldItem.getComponent("durability").damage + 1;
+        container.setItem(player.selectedSlot, newItem);
+        //When we don't have the tag, we stop the tick event.
+        if(!player.hasTag("iron_spear")){
+        world.events.tick.unsubscribe(e);
+      }}
+    })}
+    })
