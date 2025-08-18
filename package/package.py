edf557704d804fdb3efb2db96e2e3c021f004be4
QuazChick <79767058+QuazChick@users.noreply.github.com>
2025-08-18 15:35:25 +0100

    Support all archive types

--- package/package.py
+++ package/package.py
@@ -24,28 +24,17 @@ for addondir in os.listdir(rootdir):
 
     os.remove(meta_file_path)
 
-    addon_type = meta.get("type")
-    
-    if addon_type == "mcaddon":
-        output_path = os.path.join(rootdir, addondir)
-        shutil.make_archive(output_path, 'zip', full_path)
-        
-        os.rename(f"{output_path}.zip", f"{output_path}.mcaddon")
-    
-    elif addon_type == "mcpack":
-        archive_root = meta.get("archive_root")
-        
-        subfolder = os.path.join(full_path, archive_root)
-        
-        if os.path.isdir(subfolder):
-            output_path = os.path.join(rootdir, f"{addondir}")
+    archive_type = meta.get("type")
+    archive_root = meta.get("archive_root")
 
-            shutil.make_archive(output_path, 'zip', subfolder)
-            os.rename(f"{output_path}.zip", f"{output_path}.mcpack")
-        else:
+    if archive_root:
+        full_archive_root = os.path.join(full_path, archive_root)
+
+        if not os.path.isdir(full_archive_root):
             print(f"{archive_root} folder missing in {addondir}")
             sys.exit(1)
-    
     else:
-        print(f"Unknown type in meta.json for {addondir}")
-        sys.exit(1)
+        full_archive_root = full_path
+    
+    shutil.make_archive(full_path, 'zip', full_archive_root)
+    os.rename(f"{full_path}.zip", f"{full_path}.{archive_type}")
